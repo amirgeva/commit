05313f9b50560323166a54e0808aa9691a7438dc
Amir Geva <amirgeva@gmail.com>
2020-12-09 08:05:40 +0200

    Ported to PyQt5

--- commit.py
+++ commit.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python
-from PyQt4 import QtCore, QtGui
+#!/usr/bin/env python3
+from PyQt5 import QtCore, QtWidgets
 import sys
 import os
 from mainwindow import MainWindow
 
+
 def main():
-    root=os.path.abspath('.')
-    if len(sys.argv)>1:
-        root=os.path.abspath(sys.argv[1])
-    app=QtGui.QApplication(sys.argv)
+    root = os.path.abspath('.')
+    if len(sys.argv) > 1:
+        root = os.path.abspath(sys.argv[1])
+    app = QtWidgets.QApplication(sys.argv)
     QtCore.QCoreApplication.setOrganizationName("MLGSoft")
     QtCore.QCoreApplication.setOrganizationDomain("mlgsoft.com")
     QtCore.QCoreApplication.setApplicationName("commit")
-    w=MainWindow(root)
+    w = MainWindow(root)
     w.show()
     app.exec_()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
